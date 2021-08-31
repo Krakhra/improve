e01fdc1838a034c192f546c65a21a3497da14e5f
krakhra <kiratrakhra@gmail.com>
2021-08-31 16:53:48 -0400

    update schema

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
 import { useState } from "react";
+import axios from "axios";
+import { GoogleLogin } from "react-google-login";
 
 function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const test = (response) => {
+    console.log(response);
+  };
+
   return (
     <div className="App">
-      <button>Login With Google</button>
+      <GoogleLogin
+        clientId="870147874229-cbh6ihfk98cc627j8lqf2q2188a8n7sl.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={test}
+        onFailure={test}
+        cookiePolicy={"single_host_origin"}
+      />
     </div>
   );
 }
