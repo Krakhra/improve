550c8a49b210d297cdf5ce4f6aee9c58c456304e
krakhra <kiratrakhra@gmail.com>
2021-12-27 22:15:00 -0500

    added journal add journal route

--- backend/routes/mood.js
+++ backend/routes/mood.js
@@ -1,8 +1,32 @@
 const express = require("express");
+const User = require("../models/Users");
 const router = express.Router();
 
-router.get("/test", (req,res) => {
-    res.send("Here Mood")
-})
+router.get("/test", (req, res) => {
+  res.send("Here Mood");
+});
 
+router.post("/addJournal", async (req, res) => {
+  const moodObj = {
+    value: 10,
+    journal: "This is a journal entry",
+  };
+
+  const googleid = req.body.params.googleid;
+
+  const mood = await User.findOne({ googleid: googleid });
+
+  mood.mood.push(moodObj);
+  mood.save();
+
+  res.send("Done");
+});
+
+router.get("/getJournals", async (req, res) => {
+  const googleid = req.body.params.googleid;
+
+  const moods = await User.findOne({ googleid: googleid });
+
+  res.send(moods.mood);
+});
 module.exports = router;
